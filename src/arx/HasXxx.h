a8a2e2229ada8fadd8cd81678940fbe7cb786d04
ru.elric <ru.elric@localhost>
2009-12-23 18:19:58 +0000

    + HasXxx.h docs. + ARX_HAS_FUNC_XXX_TRAIT_DEF extended to support static member function & function with restrictions on result type. * classnew_allocator now uses routines from HasXxx to detect member operators new & delete.

--- src/arx/HasXxx.h
+++ src/arx/HasXxx.h
@@ -7,20 +7,41 @@
 #include <boost/mpl/bool.hpp>
 #include <boost/type_traits/is_class.hpp>
 
+// -------------------------------------------------------------------------- //
+// ARX_HAS_TYPE_XXX_TRAIT_DEF
+// -------------------------------------------------------------------------- //
 #define ARX_HAS_TYPE_XXX_TRAIT_DEF(member_name)                                 \
   BOOST_MPL_HAS_XXX_TRAIT_DEF(member_name)
 
+/** Generates a trait that returns whether the given type has a nested type with the given name. 
+ *
+ * @param trait_name                   Trait name.
+ * @param member_name                  Name of a type to check. */
 #define ARX_HAS_TYPE_XXX_TRAIT_NAMED_DEF(trait_name, member_name)               \
   BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(trait_name, member_name, false)
 
-#define ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF(trait_name, func_name, func_params) \
+
+// -------------------------------------------------------------------------- //
+// ARX_HAS_FUNC_XXX_TRAIT_DEF
+// -------------------------------------------------------------------------- //
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_DELC_PREFIX_normal() 
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_DELC_PREFIX_static() static
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_DELC_PREFIX(func_type)                  \
+  BOOST_PP_CAT(ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_DELC_PREFIX_, func_type)
+
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_POINTER_DECL_normal(type) type::*
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_POINTER_DECL_static(type) *
+#define ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_POINTER_DECL(func_type)                  \
+  BOOST_PP_CAT(ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_POINTER_DECL_, func_type)
+
+#define ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF_I(trait_name, func_name, func_return_type, func_params, decl_prefix_macro, pointer_decl_macro) \
 struct BOOST_PP_CAT(trait_name, _helper) {                                      \
   typedef char true_type;                                                       \
   struct false_type { true_type dummy[2]; };                                    \
   template<class T, T> struct wrapper {};                                       \
                                                                                 \
   struct mixin {                                                                \
-    void func_name func_params {}                                               \
+    decl_prefix_macro() func_return_type func_name func_params {}               \
   };                                                                            \
                                                                                 \
   template<class T> struct mixed {                                              \
@@ -28,7 +49,7 @@ struct BOOST_PP_CAT(trait_name, _helper) {                                      
   };                                                                            \
                                                                                 \
   template<class T>                                                             \
-  static false_type has_member(T* u, wrapper<void (mixin::*)(), &mixed<T>::type::func_name>* = 0); \
+  static false_type has_member(T* u, wrapper<func_return_type (pointer_decl_macro(mixin)) func_params, &mixed<T>::type::func_name>* = 0); \
   static true_type has_member(...);                                             \
 };                                                                              \
                                                                                 \
@@ -42,8 +63,27 @@ struct trait_name: public                                                       
 template<class T>                                                               \
 struct trait_name<T, false>: public boost::mpl::false_ {};
 
+
+/** Generates a trait that returns whether the given type has a member function with the given name. 
+ * Note that it returns true even if that function is not accessible (i.e. private).
+ *
+ * @param trait_name                   Trait name.
+ * @param func_name                    Name of a function to check.
+ * @param func_type                    Type of a function, must be either static or normal.
+ * @param func_return_type             Return type of a checking function. Is meaningful only if C++ constrains the possible return type of the function being cheched (as in case of operator new).
+ * @param func_params                  Parameters of a checking function, in parentheses. */
+#define ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF(trait_name, func_name, func_type, func_return_type, func_params) \
+  ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF_I(                                  \
+    trait_name,                                                                 \
+    func_name,                                                                  \
+    func_return_type,                                                           \
+    func_params,                                                                \
+    ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_DELC_PREFIX(func_type),                     \
+    ARX_HAS_FUNC_XXX_TRAIT_FUNCTYPE_POINTER_DECL(func_type)                     \
+  )
+
 #define ARX_HAS_FUNC_XXX_TRAIT_NAMED_DEF(trait_name, func_name)                 \
-  ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF(trait_name, func_name, ())
+  ARX_HAS_FUNC_XXX_TRAIT_NAMED_EXTENDED_DEF(trait_name, func_name, normal, void, ())
 
 #define ARX_HAS_FUNC_XXX_TRAIT_DEF(member_name)                                 \
   ARX_HAS_FUNC_XXX_TRAIT_NAMED_DEF(BOOST_PP_CAT(has_, member_name), member_name)
