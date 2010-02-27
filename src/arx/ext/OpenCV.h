befeb0f7e924bddd19186d1c5e4b6d3ee9923e69
ru.elric <ru.elric@localhost>
2010-02-27 21:51:45 +0000

    * Now alpha component is initialized to 'opaque' in vigra accessors. + Vigra <-> Magick image conversion. + Vigra <-> OpenCV image conversion (only cv::Mat is supported). + vigra::pyramidSegment (via cvPyrSegmentation). + vigra::quantize (via Magick::Image::quantize).

---
+++ src/arx/ext/OpenCV.h
@@ -0,0 +1,14 @@
+#ifndef __ARX_EXT_OPENCV_H__
+#define __ARX_EXT_OPENCV_H__
+
+#include "config.h"
+#include <opencv/cv.h>
+
+// -------------------------------------------------------------------------- //
+// Inter-lib defines
+// -------------------------------------------------------------------------- //
+#ifndef ARX_USE_OPENCV
+#  define ARX_USE_OPENCV
+#endif
+
+#endif // __ARX_EXT_OPENCV_H__
--- src/arx/ext/VigraMagick.h
+++ src/arx/ext/VigraMagick.h
@@ -2,24 +2,92 @@
 #define __ARX_EXT_VIGRA_MAGICK_H__
 
 #include "config.h"
+#include <boost/mpl/integral_c.hpp>
+#include <cassert>
 #include <fstream>
 #include "Vigra.h"
 #include "Magick.h"
 
-namespace vigra {
+namespace arx {
   namespace detail {
-    void readMagickImage(Magick::Image& magickImage, const std::string& fileName) {
+// -------------------------------------------------------------------------- //
+// magick_storage_type
+// -------------------------------------------------------------------------- //
+    template<class T> struct magick_storage_type;
+    template<> struct magick_storage_type<unsigned char>:  boost::mpl::integral_c<Magick::StorageType, Magick::CharPixel> {};
+    template<> struct magick_storage_type<double>:         boost::mpl::integral_c<Magick::StorageType, Magick::DoublePixel> {};
+    template<> struct magick_storage_type<float>:          boost::mpl::integral_c<Magick::StorageType, Magick::FloatPixel> {};
+    template<> struct magick_storage_type<unsigned int>:   boost::mpl::integral_c<Magick::StorageType, Magick::IntegerPixel> {};
+    // template<> struct magick_storage_type<unsigned long>:  boost::mpl::integral_c<Magick::StorageType, Magick::LongPixel> {}; /* MSVC chokes here for no apparent reason. */
+    template<> struct magick_storage_type<unsigned short>: boost::mpl::integral_c<Magick::StorageType, Magick::ShortPixel> {};
+    
+    template<class ChannelType, unsigned redIndex, unsigned greenIndex, unsigned blueIndex> 
+    struct magick_storage_type<vigra::RGBValue<ChannelType, redIndex, greenIndex, blueIndex> >: magick_storage_type<ChannelType> {};
+
+    template<class ChannelType> 
+    struct magick_storage_type<vigra::TinyVector<ChannelType, 4> >: magick_storage_type<ChannelType> {};
+
+
+// -------------------------------------------------------------------------- //
+// magick_order_map
+// -------------------------------------------------------------------------- //
+    template<class ChannelType> struct magick_order_map                                         { static const char* value() { return "I"; } };
+    template<class ChannelType> struct magick_order_map<vigra::RGBValue<ChannelType, 0, 1, 2> > { static const char* value() { return "RGB"; } };
+    template<class ChannelType> struct magick_order_map<vigra::RGBValue<ChannelType, 2, 1, 0> > { static const char* value() { return "BGR"; } };
+    template<class ChannelType> struct magick_order_map<vigra::TinyVector<ChannelType, 4> >     { static const char* value() { return "RGBA"; } };
+
+  } // namespace detail
+
+
+// -------------------------------------------------------------------------- //
+// Image conversion
+// -------------------------------------------------------------------------- //
+  template<class PixelType>
+  void convert(const vigra::BasicImage<PixelType>& vigraImage, Magick::Image& magickImage) {
+    if(vigraImage.width() == 0 || vigraImage.height() == 0) {
+      magickImage = Magick::Image();
+    } else {
+      magickImage.read(
+        vigraImage.width(), 
+        vigraImage.height(), 
+        detail::magick_order_map<PixelType>::value(), 
+        detail::magick_storage_type<PixelType>::value, 
+        vigraImage.data()
+      );
+    }
+  }
+
+  template<class PixelType>
+  void convert(Magick::Image& magickImage, vigra::BasicImage<PixelType>& vigraImage) {
+    if(magickImage.columns() == 0 || magickImage.rows() == 0) {
+      vigraImage = vigra::BasicImage<PixelType>();
+    } else {
+      vigraImage.resize(magickImage.columns(), magickImage.rows());
+      magickImage.write(
+        0,
+        0,
+        magickImage.columns(),
+        magickImage.rows(),
+        detail::magick_order_map<PixelType>::value(),
+        detail::magick_storage_type<PixelType>::value,
+        const_cast<vigra::BasicImage<PixelType>::pointer>(vigraImage.data()) /* We cheat a bit here, but I see no other way that would not degrade performance. */
+      );
+    }
+  }
+
+
+  namespace detail {
+// -------------------------------------------------------------------------- //
+// Image import
+// -------------------------------------------------------------------------- //
+    void importMagickImage(Magick::Image& magickImage, const std::string& fileName) {
       try {
         magickImage.read(fileName);
       } catch (Magick::Warning&) {} /* Ignore warnings. */
     }
 
-    void writeMagickImage(Magick::Image& magickImage, const std::string& fileName) {
-      magickImage.write(fileName);
-    }
-
 #ifdef ARX_WIN32
-    void readMagickImage(Magick::Image& magickImage, const std::wstring& fileName) {
+    void importMagickImage(Magick::Image& magickImage, const std::wstring& fileName) {
       std::ifstream file(fileName.c_str(), std::ios_base::in | std::ios_base::binary);
       file.seekg(0, std::ios_base::end);
       std::ifstream::pos_type fileSize = file.tellg();
@@ -36,8 +104,28 @@ namespace vigra {
         magickImage.read(magickBlob);
       } catch (Magick::Warning&) {} /* Ignore warnings. */
     }
+#endif // ARX_WIN32
 
-    void writeMagickImage(Magick::Image& magickImage, const std::wstring& fileName) {
+  } // namespace detail
+
+  template<class PixelType, class StdString>
+  void importImage(vigra::BasicImage<PixelType>& vigraImage, const StdString& fileName) {
+    Magick::Image magickImage;
+    detail::importMagickImage(magickImage, fileName);
+    convert(magickImage, vigraImage);
+  }
+
+
+  namespace detail {
+// -------------------------------------------------------------------------- //
+// Image export
+// -------------------------------------------------------------------------- //
+    void exportMagickImage(Magick::Image& magickImage, const std::string& fileName) {
+      magickImage.write(fileName);
+    }
+
+#ifdef ARX_WIN32
+    void exportMagickImage(Magick::Image& magickImage, const std::wstring& fileName) {
       Magick::Blob blob;
 
       std::string shortFileName;
@@ -52,117 +140,51 @@ namespace vigra {
       std::ofstream file(fileName.c_str(), std::ios_base::out | std::ios_base::binary);
       file.write(static_cast<const char*>(magickBlob.data()), magickBlob.length());
     }
-#endif // _WIN32
-
-// -------------------------------------------------------------------------- //
-// ImageImporter
-// -------------------------------------------------------------------------- //
-    template<class PixelType>
-    class ImageImporter {
-    public:
-      template<class StdString>
-      void operator() (BasicImage<PixelType>& vigraImage, const StdString& fileName) {
-        BRGBAImage vigraRgbaImage;
-        ImageImporter<BRGBAImage::value_type>()(vigraRgbaImage, fileName);
-
-        vigraImage.resize(vigraRgbaImage.width(), vigraRgbaImage.height());
-        copyImage(srcImageRange(vigraRgbaImage, RGBAToGrayAccessor<BRGBAImage::value_type>()), destImage(vigraImage));
-      }
-    };
-
-    template<class ChannelType, unsigned redIndex, unsigned greenIndex, unsigned blueIndex>
-    class ImageImporter<RGBValue<ChannelType, redIndex, greenIndex, blueIndex> > {
-    public:
-      template<class StdString>
-      void operator() (BasicImage<RGBValue<ChannelType, redIndex, greenIndex, blueIndex> >& vigraImage, const StdString& fileName) {
-        BRGBAImage vigraRgbaImage;
-        ImageImporter<BRGBAImage::value_type>()(vigraRgbaImage, fileName);
-
-        vigraImage.resize(vigraRgbaImage.width(), vigraRgbaImage.height());
-        copyImage(srcImageRange(vigraRgbaImage, RGBAToRGBAccessor<BRGBAImage::value_type>()), destImage(vigraImage));
-      }
-    };
-
-    template<>
-    class ImageImporter<BRGBAImage::value_type> {
-    public:
-      template<class StdString>
-      void operator() (BRGBAImage& vigraImage, const StdString& fileName) {
-        STATIC_ASSERT((sizeof(Magick::PixelPacket) == sizeof(BRGBAImage::value_type))); /* TODO. */
-
-        Magick::Image magickImage;
-
-        detail::readMagickImage(magickImage, fileName);
-
-        Magick::Pixels view(magickImage);
-        Magick::PixelPacket* pixels = view.get(0, 0, magickImage.columns(), magickImage.rows());
-
-        BRGBAImage vigraRgbaImage(magickImage.columns(), magickImage.rows(), reinterpret_cast<BRGBAImage::value_type*>(pixels));
-        vigraImage.swap(vigraRgbaImage);
-      }
-    };
-
-
-// -------------------------------------------------------------------------- //
-// ImageExporter
-// -------------------------------------------------------------------------- //
-    template<class PixelType>
-    class ImageExporter {
-    public:
-      template<class StdString>
-      void operator() (BasicImage<PixelType>& vigraImage, const StdString& fileName) {
-        BRGBAImage vigraRgbaImage;
-        vigraRgbaImage.resize(vigraImage.width(), vigraImage.height());
-        copyImage(srcImageRange(vigraImage, GrayToRGBAAccessor<PixelType>()), destImage(vigraRgbaImage));
-        ImageExporter<BRGBAImage::value_type>()(vigraRgbaImage, fileName);
-      }
-    };
-
-    template<class ChannelType, unsigned redIndex, unsigned greenIndex, unsigned blueIndex>
-    class ImageExporter<RGBValue<ChannelType, redIndex, greenIndex, blueIndex> > {
-    public:
-      template<class StdString>
-      void operator() (BasicImage<RGBValue<ChannelType, redIndex, greenIndex, blueIndex> >& vigraImage, const StdString& fileName) {
-        BRGBAImage vigraRgbaImage;
-        vigraRgbaImage.resize(vigraImage.width(), vigraImage.height());
-        copyImage(srcImageRange(vigraImage, RGBToRGBAAccessor<RGBValue<ChannelType, redIndex, greenIndex, blueIndex> >()), destImage(vigraRgbaImage));
-        ImageExporter<BRGBAImage::value_type>()(vigraRgbaImage, fileName);
-      }
-    };
-
-    template<>
-    class ImageExporter<BRGBAImage::value_type> {
-    public:
-      template<class StdString>
-      void operator() (BRGBAImage& vigraImage, const StdString& fileName) {
-        STATIC_ASSERT((sizeof(Magick::PixelPacket) == sizeof(BRGBAImage::value_type))); /* TODO. */
-
-        Magick::Image magickImage(Magick::Geometry(vigraImage.width(), vigraImage.height()), Magick::Color(0, 0, 0, 0));
-
-        Magick::Pixels view(magickImage);
-        Magick::PixelPacket* pixels = view.get(0, 0, vigraImage.width(), vigraImage.height());
-
-        memcpy(pixels, vigraImage.data(), sizeof(BRGBAImage::value_type) * vigraImage.width() * vigraImage.height());
-
-        magickImage.syncPixels();
-
-        detail::writeMagickImage(magickImage, fileName);
-      }
-
-    };
+#endif // ARX_WIN32
 
   } // namespace detail
 
   template<class PixelType, class StdString>
-  void exportImage(BasicImage<PixelType>& vigraImage, const StdString& fileName) {
-    detail::ImageExporter<PixelType>()(vigraImage, fileName);
+  void exportImage(const vigra::BasicImage<PixelType>& vigraImage, const StdString& fileName) {
+    Magick::Image magickImage;
+    convert(vigraImage, magickImage);
+    detail::exportMagickImage(magickImage, fileName);
   }
 
-  template<class PixelType, class StdString>
-  void importImage(BasicImage<PixelType>& vigraImage, const StdString& fileName) {
-    detail::ImageImporter<PixelType>()(vigraImage, fileName);
+
+// -------------------------------------------------------------------------- //
+// Quantization
+// -------------------------------------------------------------------------- //
+  template<class PixelType>
+  void quantize(BasicImage<PixelType>& vigraImage, unsigned colors) {
+    Magick::Image magickImage;
+    convert(vigraImage, magickImage);
+
+    magickImage.quantizeColors(colors);
+    magickImage.quantizeColorSpace(RGBColorspace);
+    magickImage.quantizeDither(false);
+    magickImage.quantize();
+
+    convert(magickImage, vigraImage);
   }
 
-} // namespace vigra
+} // namespace arx
+
+// -------------------------------------------------------------------------- //
+// Export defined symbols to enable ADL
+// -------------------------------------------------------------------------- //
+namespace vigra {
+  using arx::convert;
+  using arx::importImage;
+  using arx::exportImage;
+  using arx::quantize;
+}
+
+namespace Magick {
+  using arx::convert;
+}
+
 
 #endif // __ARX_EXT_VIGRA_MAGICK_H__
+
+
