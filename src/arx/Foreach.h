ecfc6c4e86eb3972109d92480b80399969d55d91
ru.elric <ru.elric@localhost>
2010-06-05 15:11:13 +0000

    * map_foreach fixed to support break.

--- src/arx/Foreach.h
+++ src/arx/Foreach.h
@@ -2,7 +2,6 @@
 #define ARX_FOREACH_H
 
 #include <boost/foreach.hpp>
-#include <boost/typeof/typeof.hpp>
 #include <boost/preprocessor/cat.hpp>
 
 /* Undefine qt foreach. */
@@ -14,13 +13,14 @@
 
 #define reverse_foreach BOOST_REVERSE_FOREACH
 
-#define ARX_FOREACH_VAR(NAME) BOOST_PP_CAT(NAME, __LINE__)
+#define ARX_FOREACH_VAR(NAME) BOOST_PP_CAT(arx_foreach_, BOOST_PP_CAT(NAME, __LINE__))
 
-/* TODO: break / continue doesn't work! */
 #define map_foreach(KEY, VAL, MAP)                                              \
-  BOOST_FOREACH(BOOST_TYPEOF(*(MAP).begin())& pair, MAP)                        \
-  for(bool ARX_FOREACH_VAR(stop) = false; !ARX_FOREACH_VAR(stop);)              \
-  for(KEY = pair.first; !ARX_FOREACH_VAR(stop);)                                \
-  for(VAL = pair.second; !ARX_FOREACH_VAR(stop); ARX_FOREACH_VAR(stop) = true)
+  for(bool ARX_FOREACH_VAR(_continue) = true; false;)                           \
+  foreach(auto pair, MAP)                                                       \
+  if(!ARX_FOREACH_VAR(_continue)) { break; } else                               \
+  for(ARX_FOREACH_VAR(_continue) = false; false;)                               \
+  for(KEY = pair.first; false;)                                                 \
+  for(VAL = pair.second; false; ARX_FOREACH_VAR(_continue) = true)
 
 #endif // ARX_FOREACH_H
