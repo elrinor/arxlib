a77566ac94aadfe1ec8fa1c0e1390208410a8d85
ru.elric <ru.elric@localhost>
2010-07-25 09:51:09 +0000

    * Awful bug in map_foreach fixed.

--- src/arx/Foreach.h
+++ src/arx/Foreach.h
@@ -16,11 +16,12 @@
 #define ARX_FOREACH_VAR(NAME) BOOST_PP_CAT(arx_foreach_, BOOST_PP_CAT(NAME, __LINE__))
 
 #define map_foreach(KEY, VAL, MAP)                                              \
-  for(bool ARX_FOREACH_VAR(_continue) = true; false;)                           \
+  if(bool ARX_FOREACH_VAR(_continue) = true)                                    \
   foreach(auto pair, MAP)                                                       \
-  if(!ARX_FOREACH_VAR(_continue)) { break; } else                               \
-  for(ARX_FOREACH_VAR(_continue) = false; false;)                               \
-  for(KEY = pair.first; false;)                                                 \
-  for(VAL = pair.second; false; ARX_FOREACH_VAR(_continue) = true)
+  if(!ARX_FOREACH_VAR(_continue)) break; else                                   \
+  if(bool ARX_FOREACH_VAR(_iteration) = true)                                   \
+  if((ARX_FOREACH_VAR(_continue) = false), true)                                \
+  for(KEY = pair.first; ARX_FOREACH_VAR(_iteration); ARX_FOREACH_VAR(_iteration) = false) \
+  for(VAL = pair.second; ARX_FOREACH_VAR(_iteration); ARX_FOREACH_VAR(_continue) = true, ARX_FOREACH_VAR(_iteration) = false)
 
 #endif // ARX_FOREACH_H
