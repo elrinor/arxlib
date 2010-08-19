793fdd78c951cb18a0cc02fafeb45331d395c918
ru.elric <ru.elric@localhost>
2010-08-19 20:27:21 +0000

    + ARX_NAMED_STATIC_BLOCK, ARX_NAMED_STATIC_INITIALIZER, ARX_NAMED_STATIC_DEINITIALIZER

--- src/arx/StaticBlock.h
+++ src/arx/StaticBlock.h
@@ -3,10 +3,17 @@
 
 #include <boost/preprocessor/cat.hpp>
 
-#define ARX_STATIC_BLOCK_I(BLOCK_NAME)                                          \
-  template<class T> class BLOCK_NAME {                                          \
+#define ARX_STATIC_BLOCK_I(BLOCK_NAME, DERIVED_BLOCK_NAME)                      \
+  class BLOCK_NAME {                                                            \
+  protected:                                                                    \
+    BLOCK_NAME() {}                                                             \
+                                                                                \
+    static inline void block();                                                 \
+  };                                                                            \
+                                                                                \
+  template<class T> class DERIVED_BLOCK_NAME: private BLOCK_NAME {              \
   public:                                                                       \
-    BLOCK_NAME() {                                                              \
+    DERIVED_BLOCK_NAME() {                                                      \
       if(!sExecuted) {                                                          \
         block();                                                                \
         sExecuted = true;                                                       \
@@ -14,18 +21,27 @@
     }                                                                           \
                                                                                 \
   private:                                                                      \
-    static inline void block();                                                 \
-                                                                                \
     static bool sExecuted;                                                      \
   };                                                                            \
                                                                                 \
-  template<> bool BLOCK_NAME<void>::sExecuted = false;                          \
+  template<> bool DERIVED_BLOCK_NAME<void>::sExecuted = false;                  \
                                                                                 \
   namespace {                                                                   \
-    BLOCK_NAME<void> BOOST_PP_CAT(BLOCK_NAME, _value);                          \
+    DERIVED_BLOCK_NAME<void> BOOST_PP_CAT(BLOCK_NAME, _value);                  \
   }                                                                             \
                                                                                 \
-  template<> void BLOCK_NAME<void>::block()                                     \
+  void BLOCK_NAME::block()
+
+
+/**
+ * Defines a java-like static block of code that resides in a class with the
+ * given name. Client code can befriend this class.
+ *
+ * @see ARX_STATIC_BLOCK
+ */
+#define ARX_NAMED_STATIC_BLOCK(BLOCK_NAME)                                      \
+  ARX_STATIC_BLOCK_I(BLOCK_NAME, BOOST_PP_CAT(BLOCK_NAME, _DERIVED))
+
 
 /**
  * Defines a java-like static block of code. 
@@ -39,40 +55,63 @@
  * @param FILE_ID unique identifier used to prevent name clashes.
  */
 #define ARX_STATIC_BLOCK(FILE_ID)                                               \
-  ARX_STATIC_BLOCK_I(BOOST_PP_CAT(StaticBlock, BOOST_PP_CAT(FILE_ID, __LINE__)))
+  ARX_NAMED_STATIC_BLOCK(BOOST_PP_CAT(StaticBlock, BOOST_PP_CAT(FILE_ID, __LINE__)))
+
+
+/**
+ * @see ARX_NAMED_STATIC_BLOCK
+ */ 
+#define ARX_NAMED_STATIC_INITIALIZER ARX_NAMED_STATIC_BLOCK
+
 
 /**
  * @see ARX_STATIC_BLOCK
  */
-#define ARX_STATIC_INITIALIZER(FILE_ID)                                         \
-  ARX_STATIC_BLOCK(FILE_ID)
+#define ARX_STATIC_INITIALIZER ARX_STATIC_BLOCK
 
-#define ARX_STATIC_DEINITIALIZER_I(BLOCK_NAME)                                  \
-  template<class T> class BLOCK_NAME {                                          \
+
+#define ARX_STATIC_DEINITIALIZER_I(BLOCK_NAME, DERIVED_BLOCK_NAME)              \
+  class BLOCK_NAME {                                                            \
+  protected:                                                                    \
+    BLOCK_NAME() {}                                                             \
+                                                                                \
+    static inline void block();                                                 \
+  };                                                                            \
+                                                                                \
+  template<class T> class DERIVED_BLOCK_NAME: private BLOCK_NAME {              \
   public:                                                                       \
-    BLOCK_NAME() {                                                              \
+    DERIVED_BLOCK_NAME() {                                                      \
       sCounter++;                                                               \
     }                                                                           \
                                                                                 \
-    ~BLOCK_NAME() {                                                             \
+    ~DERIVED_BLOCK_NAME() {                                                     \
       sCounter--;                                                               \
       if(sCounter == 0)                                                         \
         block();                                                                \
     }                                                                           \
                                                                                 \
   private:                                                                      \
-    static inline void block();                                                 \
-                                                                                \
     static int sCounter;                                                        \
   };                                                                            \
                                                                                 \
-  template<> int BLOCK_NAME<void>::sCounter = 0;                                \
+  template<> int DERIVED_BLOCK_NAME<void>::sCounter = 0;                        \
                                                                                 \
   namespace {                                                                   \
-    BLOCK_NAME<void> BOOST_PP_CAT(BLOCK_NAME, _value);                          \
+    DERIVED_BLOCK_NAME<void> BOOST_PP_CAT(BLOCK_NAME, _value);                  \
   }                                                                             \
                                                                                 \
-  template<> void BLOCK_NAME<void>::block()
+  void BLOCK_NAME::block()
+
+
+/**
+ * Defines a deinitialization block of code that resides in a class with the
+ * given name. Client code can befriend this class.
+ *
+ * @see ARX_STATIC_DEINITIALIZER
+ */
+#define ARX_NAMED_STATIC_DEINITIALIZER(CLASS_NAME)                              \
+  ARX_STATIC_DEINITIALIZER_I(CLASS_NAME, BOOST_PP_CAT(CLASS_NAME, _DERIVED))
+
 
 /**
  * Defines a deinitialization block of code.
@@ -86,6 +125,7 @@
  * @param FILE_ID unique identifier used to prevent name clashes.
  */
 #define ARX_STATIC_DEINITIALIZER(FILE_ID)                                       \
-  ARX_STATIC_DEINITIALIZER_I(BOOST_PP_CAT(StaticDeinitializer, BOOST_PP_CAT(FILE_ID, __LINE__)))
+  ARX_NAMED_STATIC_DEINITIALIZER(BOOST_PP_CAT(StaticDeinitializer, BOOST_PP_CAT(FILE_ID, __LINE__)))
+
 
 #endif // ARX_STATIC_BLOCK_H
