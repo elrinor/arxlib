f63008076487254e5c106d2a68ead84eb4bee613
ru.elric <ru.elric@localhost>
2010-08-28 20:29:53 +0000

    + Truly evil xml data binding framework.

--- src/arx/ext/qt/XmlPath.h
+++ src/arx/ext/qt/XmlPath.h
@@ -1,14 +1,33 @@
+/* This file is part of ArXLib, a C++ ArX Primitives Library.
+ *
+ * Copyright (C) 2008-2010 Alexander Fokin <apfokin@gmail.com>
+ *
+ * ArXLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * ArXLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$ */
 #ifndef ARX_EXT_QT_XML_PATH_H
 #define ARX_EXT_QT_XML_PATH_H
 
 #include "config.h"
+#include <boost/mpl/bool.hpp>
+#include <boost/mpl/or.hpp>
 #include <boost/proto/proto.hpp>
 #include "DefaultNodeWalker.h"
 
 namespace arx { namespace xml {
   namespace detail {
     namespace proto = boost::proto;
-    namespace fusion = boost::fusion;
     namespace mpl = boost::mpl;
 
     /**
