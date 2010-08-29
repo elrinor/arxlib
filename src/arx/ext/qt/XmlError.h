63ac152c06428965dd911f3078429afb124aefb2
ru.elric <ru.elric@localhost>
2010-08-29 14:13:38 +0000

    * Decoupled xml binding engine from Qt.

---
+++ src/arx/ext/qt/XmlError.h
@@ -0,0 +1,112 @@
+/* This file is part of ArXLib, a C++ ArX Primitives Library.
+ *
+ * Copyright (C) 2008-2010 Alexander Fokin <apfokin@gmail.com>
+ *
+ * ArXLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * ArXLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$ */
+#ifndef ARX_EXT_QT_XML_ERROR_DATA_H
+#define ARX_EXT_QT_XML_ERROR_DATA_H
+
+#include "config.h"
+
+namespace arx { namespace xml {
+  /**
+   * Error severity.
+   */
+  enum ErrorSeverity {
+    ERROR,
+    WARNING
+  };
+
+
+  /**
+   * Error location.
+   */
+  class ErrorLocation {
+  public:
+    ErrorLocation(int line, int column): mLine(line), mColumn(column) {}
+
+    int line() const {
+      return mLine;
+    }
+
+    int column() const {
+      return mColumn;
+    }
+
+  private:
+    int mLine, mColumn;
+  };
+
+
+  /**
+   * Base class for all xml errors.
+   */
+  struct error {};
+
+
+  /**
+   * Node not found error.
+   */
+  template<class String>
+  struct node_not_found: error {
+    node_not_found(const String &path):
+      path(path) {}
+
+    const String &path;
+  };
+
+  template<class String>
+  node_not_found<String> create_node_not_found(const String &path) {
+    return node_not_found<String>(path);
+  }
+
+
+  /**
+   * Invalid value error.
+   */
+  template<class String>
+  struct invalid_value: error {
+    invalid_value(const String &value): 
+      value(value) {}
+
+    const String &value;
+  };
+
+  template<class String>
+  invalid_value<String> create_invalid_value(const String &value) {
+    return invalid_value<String>(value)
+  }
+
+
+  /**
+   * Typed invalid value error.
+   */
+  template<class T, class String>
+  struct invalid_value_for_type: invalid_value<String> {
+    invalid_value_for_type(const String &value):
+      invalid_value(value) {};
+
+    typedef T type;
+  };
+
+  template<class T, class String>
+  invalid_value_for_type<T, String> create_invalid_value_for_type(const String &value) {
+    return invalid_value_for_type<T, String>(value);
+  }
+
+}} // namespace arx::xml
+
+#endif // ARX_EXT_QT_XML_ERROR_DATA_H
---
+++ src/arx/ext/qt/XmlNodeInspector.h
@@ -0,0 +1,99 @@
+/* This file is part of ArXLib, a C++ ArX Primitives Library.
+ *
+ * Copyright (C) 2008-2010 Alexander Fokin <apfokin@gmail.com>
+ *
+ * ArXLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * ArXLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$ */
+#ifndef ARX_EXT_QT_XML_NODE_INSPECTOR_H
+#define ARX_EXT_QT_XML_NODE_INSPECTOR_H
+
+#include "config.h"
+#include "XmlError.h"
+
+namespace arx { namespace xml {
+  namespace xml_node_inspector_detail {
+// -------------------------------------------------------------------------- //
+// Wrappers
+// -------------------------------------------------------------------------- //
+    template<class Node>
+    struct name_type {
+      typedef decltype(name(*static_cast<const Node *>(NULL))) type;
+    };
+
+    template<class Node>
+    typename name_type<Node>::type
+    name_wrapper(const Node &node) {
+      return name(node);
+    }
+
+    template<class Node>
+    struct value_type {
+      typedef decltype(value(*static_cast<const Node *>(NULL))) type;
+    };
+
+    template<class Node>
+    typename value_type<Node>::type
+    value_wrapper(const Node &node) {
+      return value(node);
+    }
+
+    template<class Node>
+    ErrorLocation location_wrapper(const Node &node) {
+      return location(node);
+    }
+
+  } // namespace xml_node_inspector_detail
+
+
+// -------------------------------------------------------------------------- //
+// DefaultNodeInspector
+// -------------------------------------------------------------------------- //
+  class DefaultNodeInspector {
+  public:
+    template<class Node>
+    typename xml_node_inspector_detail::name_type<Node>::type
+    name(const Node &node) const {
+      return xml_node_inspector_detail::name_wrapper(node);
+    }
+
+    template<class Node>
+    typename xml_node_inspector_detail::value_type<Node>::type
+    value(const Node &node) const {
+      return xml_node_inspector_detail::value_wrapper(node);
+    }
+
+    template<class Node>
+    ErrorLocation location(const Node &node) const {
+      retunr xml_node_inspector_detail::location_wrapper(node);
+    }
+
+  };
+
+
+// -------------------------------------------------------------------------- //
+// node_inspector
+// -------------------------------------------------------------------------- //
+  /** 
+   * Specialize for your node type, or define global functions that can be 
+   * found via ADL.
+   */
+  template<class Node>
+  struct node_inspector {
+    typedef DefaultNodeInspector type;
+  };
+
+}} // namespace arx::xml
+
+#endif // ARX_EXT_QT_XML_NODE_INSPECTOR_H
