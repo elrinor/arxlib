8f5183faa2cd566e9931221df1ff270c0e34b9de
ru.elric <ru.elric@localhost>
2010-09-23 14:59:40 +0000

    + CMake tests.

---
+++ test/arx/Foreach.cpp
@@ -0,0 +1,65 @@
+/* This file is part of ArXLib, a C++ ArX Primitives Library.
+ *
+ * Copyright (C) 2008-2010 Alexander Fokin <apfokin@gmail.com>
+ *
+ * ArXLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * ArXLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$ */
+#define NOMINMAX
+#define BOOST_TEST_MODULE arx_foreach
+#include <boost/test/included/unit_test.hpp>
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <arx/Foreach.h>
+
+BOOST_AUTO_TEST_CASE(arx_foreach) {
+
+  /* Tests for map_foreach. Checks break & continue. */
+  {
+    std::map<int, int> m;
+    m[0] = 1;
+    m[1] = 10;
+    m[2] = 100;
+    m[3] = 1000;
+
+    int sum = 0, keysum = 0;
+    map_foreach(int key, int value, m) {
+      sum += value;
+      keysum += key;
+    }
+    BOOST_CHECK_EQUAL(sum, 1111);
+    BOOST_CHECK_EQUAL(keysum, 6);
+
+    sum = 0;
+    map_foreach(int key, int value, m) {
+      if(key < 2)
+        continue;
+      sum += value;
+    }
+    BOOST_CHECK_EQUAL(sum, 1100);
+
+    sum = 0;
+    int max = 0;
+    map_foreach(int key, int value, m) {
+      if(value > 100)
+        break;
+      max = std::max(max, value);
+    }
+    BOOST_CHECK(max <= 100);
+
+  }
+
+}
--- test/arx/HasXxx.cpp
+++ test/arx/HasXxx.cpp
@@ -16,13 +16,41 @@
  * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
  * 
  * $Id$ */
-#include <arx/config.h>
-#include <QtPlugin>
+#define NOMINMAX
+#define BOOST_TEST_MODULE arx_has_xxx
+#include <boost/test/included/unit_test.hpp>
 
-#ifdef ARX_QT_STATIC_PLUGINS
-Q_IMPORT_PLUGIN(qjpeg);
-Q_IMPORT_PLUGIN(qgif);
-Q_IMPORT_PLUGIN(qtiff);
-Q_IMPORT_PLUGIN(qmng);
-Q_IMPORT_PLUGIN(qico);
-#endif // ARX_QT_STATIC_PLUGINS
+#include <arx/HasXxx.h>
+
+struct C1 {
+  typedef int type;
+};
+
+struct C2 {
+  int type();
+};
+
+ARX_HAS_TYPE_XXX_TRAIT_NAMED_DEF(has_type, type);
+
+
+struct C3 {
+  void f();
+  void f(int);
+};
+
+struct C4 {
+  void g();
+};
+
+ARX_HAS_FUNC_XXX_TRAIT_DEF(f);
+
+
+BOOST_AUTO_TEST_CASE(arx_has_xxx) {
+
+  BOOST_CHECK(has_type<C1>::value);
+  BOOST_CHECK(!has_type<C2>::value);
+
+  BOOST_CHECK(has_f<C3>::value);
+  BOOST_CHECK(!has_f<C4>::value);
+
+}
