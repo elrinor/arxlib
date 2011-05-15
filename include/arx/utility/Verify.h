5d111d2e86cb58c2dfc54ceb029d1d0eb13d5181
ru.elric <ru.elric@localhost>
2011-05-15 15:54:58 +0000

    + fatal() and verify(expr) macros.

---
+++ include/arx/utility/Verify.h
@@ -0,0 +1,31 @@
+/* This file is part of ArXLib, a C++ ArX Primitives Library.
+ *
+ * Copyright (C) 2008-2011 Alexander Fokin <apfokin@gmail.com>
+ *
+ * ArXLib is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * ArXLib is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ArXLib. If not, see <http://www.gnu.org/licenses/>. 
+ * 
+ * $Id$ */
+#ifndef ARX_UTILITY_VERIFY_H
+#define ARX_UTILITY_VERIFY_H
+
+#include <arx/config.h>
+#include <cassert>
+
+#ifdef NDEBUG
+#  define verify(expr) ((void)(expr))
+#else
+#  define verify(expr) assert(expr)
+#endif
+
+#endif // ARX_UTILITY_VERIFY_H
