6148380024414a752fd176df76cd6a9da7620286
ru.elric <ru.elric@localhost>
2011-05-19 18:08:05 +0000

    * Fixed forgotten inline.

--- include/arx/utility/Unused.h
+++ include/arx/utility/Unused.h
@@ -22,22 +22,22 @@
 #include <arx/config.h>
 
 namespace arx { namespace detail {
-  void mark_unused() {}
+  inline void mark_unused() {}
 
   template<class T0>
-  void mark_unused(const T0 &) {}
+  inline void mark_unused(const T0 &) {}
 
   template<class T0, class T1>
-  void mark_unused(const T0 &, const T1 &) {}
+  inline void mark_unused(const T0 &, const T1 &) {}
 
   template<class T0, class T1, class T2>
-  void mark_unused(const T0 &, const T1 &, const T2 &) {}
+  inline void mark_unused(const T0 &, const T1 &, const T2 &) {}
 
   template<class T0, class T1, class T2, class T3>
-  void mark_unused(const T0 &, const T1 &, const T2 &, const T3 &) {}
+  inline void mark_unused(const T0 &, const T1 &, const T2 &, const T3 &) {}
 
   template<class T0, class T1, class T2, class T3, class T4>
-  void mark_unused(const T0 &, const T1 &, const T2 &, const T3 &, const T4 &) {}
+  inline void mark_unused(const T0 &, const T1 &, const T2 &, const T3 &, const T4 &) {}
 
 }} // namespace arx::detail
 
